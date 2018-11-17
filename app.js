6866b64f333a7d0d737a35c4b6033424bc33947a
Robert-Furth <44911447+Robert-Furth@users.noreply.github.com>
2018-11-17 14:03:08 -0800

    Added some minimal functionality

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Just testing...'));
+const home = '/index.html'
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + home)));
+
+app.get('/:location', (req, res) => {
+	search_term = req.params.location;
+	console.log(search_term);
+});
+
+app.listen(port, () => console.log('Example app listening on port ${port}!'));
